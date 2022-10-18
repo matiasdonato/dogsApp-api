5284c4b5f8abb41ab807833f18718c1f08814729
Matias <matiasdonato14@gmail.com>
2022-10-18 04:32:18 -0300

    make it better

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let app = require("./src/server");
 let { db } = require("./src/db/index.js")
-let PORT = 3001;
+let LOCALPORT = 3001;
 
 
 db.sync({ force: false }).then(() => {
-    app.listen(PORT, () => {
+    app.listen(process.env.PORT || LOCALPORT, () => {
         console.log(`server listening on port ${PORT}`);
     })
 })
