575290584c0fdad01071a3c7aa6e183528ed3eba
Matias <matiasdonato14@gmail.com>
2022-10-18 19:00:42 -0300

    make it better

--- src/storage/storage.js
+++ src/storage/storage.js
@@ -2,7 +2,7 @@ const multer = require("multer")
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, './src/storage')
+        cb(null, './src/storage/imgs')
     },
     filename: function(req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
