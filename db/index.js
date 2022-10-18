e1e8c3b21cb0b32c085da5e4d55e3945c68226db
Matias <matiasdonato14@gmail.com>
2022-10-18 04:16:17 -0300

    make it better

--- db/index.js
+++ db/index.js
@@ -2,15 +2,19 @@ let { Sequelize, Op } = require("sequelize");
 let modelBreed = require("./models/Breed.js")
 let modelTemper = require("./models/Temper.js")
 
-const db = new Sequelize('d4shnt4ivtrd0p', 'riciakmvlxhejf', '0cc2813ea72c9900c9202d4c25834cb65b54e56cab67b639f8cb9dc62de5c666', {
-    host: 'ec2-44-210-228-110.compute-1.amazonaws.com',
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false,
-        }
-    }
+// const db = new Sequelize('d4shnt4ivtrd0p', 'riciakmvlxhejf', '0cc2813ea72c9900c9202d4c25834cb65b54e56cab67b639f8cb9dc62de5c666', {
+//     host: 'ec2-44-210-228-110.compute-1.amazonaws.com',
+//     dialect: 'postgres',
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false,
+//         }
+//     }
+// });
+
+const db = new Sequelize('postgres://matiasdonato:dona090504:5432/dogs', {
+    logging: false,
 });
 
 //new Sequelize('postgres://riciakmvlxhejf:0cc2813ea72c9900c9202d4c25834cb65b54e56cab67b639f8cb9dc62de5c666@lec2-44-210-228-110.compute-1.amazonaws.com:5432/d4shnt4ivtrd0p', {
