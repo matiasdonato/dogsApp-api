3bc4a3d5fde9d5e7ad6fc61930cf059b757c419e
Matias <matiasdonato14@gmail.com>
2022-10-18 04:37:19 -0300

    make it better

--- src/db/index.js
+++ src/db/index.js
@@ -6,20 +6,20 @@ let modelTemper = require("./models/Temper.js")
 //     "start": "node index.js"
 // }
 
-// const db = new Sequelize('d4shnt4ivtrd0p', 'riciakmvlxhejf', '0cc2813ea72c9900c9202d4c25834cb65b54e56cab67b639f8cb9dc62de5c666', {
-//     host: 'ec2-44-210-228-110.compute-1.amazonaws.com',
-//     dialect: 'postgres',
-//     dialectOptions: {
-//         ssl: {
-//             require: true,
-//             rejectUnauthorized: false,
-//         }
-//     }
-// });
-
-const db = new Sequelize('postgres://matiasdonato:dona090504@localhost/dogs', {
-    logging: false,
+const db = new Sequelize('d4shnt4ivtrd0p', 'riciakmvlxhejf', '0cc2813ea72c9900c9202d4c25834cb65b54e56cab67b639f8cb9dc62de5c666', {
+    host: 'ec2-44-210-228-110.compute-1.amazonaws.com',
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        }
+    }
 });
+
+// const db = new Sequelize('postgres://matiasdonato:dona090504@localhost/dogs', {
+//     logging: false,
+// });
 
 //new Sequelize('postgres://riciakmvlxhejf:0cc2813ea72c9900c9202d4c25834cb65b54e56cab67b639f8cb9dc62de5c666@lec2-44-210-228-110.compute-1.amazonaws.com:5432/d4shnt4ivtrd0p', {
 //     logging: false,
