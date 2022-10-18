6367bf8d4b0393c84c4f1bcacc4f2a64d44c04b0
Matias <matiasdonato14@gmail.com>
2022-10-18 04:29:04 -0300

    make it better

--- src/db/index.js
+++ src/db/index.js
@@ -2,6 +2,10 @@ let { Sequelize, Op } = require("sequelize");
 let modelBreed = require("./models/Breed.js")
 let modelTemper = require("./models/Temper.js")
 
+// "scripts": {
+//     "start": "node index.js"
+// }
+
 // const db = new Sequelize('d4shnt4ivtrd0p', 'riciakmvlxhejf', '0cc2813ea72c9900c9202d4c25834cb65b54e56cab67b639f8cb9dc62de5c666', {
 //     host: 'ec2-44-210-228-110.compute-1.amazonaws.com',
 //     dialect: 'postgres',
