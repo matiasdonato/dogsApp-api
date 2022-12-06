9d7ba0badbd88ca85c758a727064b9543f4c987c
Matias <matiasdonato14@gmail.com>
2022-12-05 23:57:50 -0300

    .

--- src/db/index.js
+++ src/db/index.js
@@ -3,28 +3,11 @@ let modelBreed = require("./models/Breed.js")
 let modelTemper = require("./models/Temper.js")
 require("dotenv").config()
 
-// "scripts": {
-//     "start": "node index.js"
-// }
 
-// const db = new Sequelize('d4shnt4ivtrd0p', 'riciakmvlxhejf', '0cc2813ea72c9900c9202d4c25834cb65b54e56cab67b639f8cb9dc62de5c666', {
-//     host: 'ec2-44-210-228-110.compute-1.amazonaws.com',
-//     dialect: 'postgres',
-//     dialectOptions: {
-//         ssl: {
-//             require: true,
-//             rejectUnauthorized: false,
-//         }
-//     }
-// });
-
-process.env.PGUSER
 
 const db = new Sequelize(`postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`);
 
-//new Sequelize('postgres://riciakmvlxhejf:0cc2813ea72c9900c9202d4c25834cb65b54e56cab67b639f8cb9dc62de5c666@lec2-44-210-228-110.compute-1.amazonaws.com:5432/d4shnt4ivtrd0p', {
-//     logging: false,
-// });
+
 
 modelBreed(db);
 modelTemper(db);
